c5095ff829f2da1bf4594c0d5fb28417c4aff2f7
DevCraft User <kaloyan.m.dimitrov@gmail.com>
2022-12-28 18:52:54 +0200

    configured data model to use lombok

--- src/main/java/guru/springframework/api/domain/Address.java
+++ src/main/java/guru/springframework/api/domain/Address.java
@@ -1,12 +1,17 @@
 
 package guru.springframework.api.domain;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Generated;
 
-@Generated("jsonschema2pojo")
+@Getter
+@Setter
 public class Address implements Serializable
 {
 
@@ -15,55 +20,10 @@ public class Address implements Serializable
     private String city;
     private String zipcode;
     private Geo geo;
+    @Setter(lombok.AccessLevel.NONE)
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
     private final static long serialVersionUID = -3734311591897393438L;
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getSuite() {
-        return suite;
-    }
-
-    public void setSuite(String suite) {
-        this.suite = suite;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public Geo getGeo() {
-        return geo;
-    }
-
-    public void setGeo(Geo geo) {
-        this.geo = geo;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
     }
-
 }
