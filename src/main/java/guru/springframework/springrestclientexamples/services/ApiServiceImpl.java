c79d0d75f4aea6f28ff0b52f2da418b2d7d683f9
DevCraft User <kaloyan.m.dimitrov@gmail.com>
2022-12-29 20:58:16 +0200

    updated api service

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,14 +13,20 @@ import java.util.List;
 @Service
 public class ApiServiceImpl implements ApiService {
     private RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer _limit) {
-        User[] userList = restTemplate.getForObject("http://jsonplaceholder.typicode.com/users?_limit=" + _limit, User[].class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", _limit);
+
+        User[] userList = restTemplate.getForObject(uriBuilder.toUriString(), User[].class);
         return Arrays.asList(userList);
     }
 }
